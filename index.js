438b8c44ca4b81f0516dd7ffefa78ab13cc64ced
Nicolas22221 <87592631+Nicolas22221@users.noreply.github.com>
2026-04-30 20:01:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ client.once('ready', async () => {
 
     client.user.setPresence({
       activities: [{
-        name: 'quero um labubu aquático 🐟',
+        name: 'grande e medonho, igual o lucas 🐟',
         type: ActivityType.Playing
       }],
       status: 'online'
